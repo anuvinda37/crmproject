6fbb9c4c2855c60f35f0c943dfaec8d6740c1e96
anuvinda37 <anuvindakv@gmail.com>
2024-03-09 20:52:52 +0530

    fourth commit

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from.import views
-urlpatterns=[
-    path('',views.home,name='home'),
-    path('dashboard',views.dashboard,name='dashboard'),
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('archive', views.archive, name='archive'),
+   
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 def dashboard(request):
-    return render(request,'dashboard.html')
+    return render(request, 'dashboard.html')
+def archive(request):
+    return render(request, 'archive.html')
+
+    

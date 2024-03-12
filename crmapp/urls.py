b81b70c7640c419a1473f122dabd5494a9c59995
anuvinda37 <anuvindakv@gmail.com>
2024-03-12 23:27:43 +0530

    fifth commit

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('archive', views.archive, name='archive'),
+     path('page1', views.page1, name='page1'),
+      path('page2', views.page2, name='page2'),
    
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    item_idf = request.GET.get('item_idf',' ').split(',')
+    return render(request, 'dashboard.html',{'item_idf':item_idf})
 def archive(request):
-    return render(request, 'archive.html')
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'archive.html', {'item_ids': item_ids}) 
 
-    
+def page1(request):
+    item_idf = request.GET.get('item_idf',' ').split(',')
+    return render(request, 'page1.html',{'item_idf':item_idf})
+
+def page2(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'page2.html', {'item_ids': item_ids}) 
